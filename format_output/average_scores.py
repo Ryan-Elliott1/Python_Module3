1bdd4cb1c8d5911b744ae882151cc65fb3be169d
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-07 19:39:32 -0500

    average function

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,2 +1,9 @@
 def average():
-    pass
+    score1 = input("Enter score 1: ")
+    score2 = input("Enter score 2: ")
+    score3 = input("Enter score 3: ")
+    return (int(score1) + int(score2) + int(score3)) / 3
+
+
+if __name__ == '__main__':
+    print(average())
